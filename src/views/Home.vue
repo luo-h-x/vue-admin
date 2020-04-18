b34d78331c94a4ae3e5ba6eb65850ff733cdec59
lhx <lhx@git.com>
2020-04-18 16:37:46 +0800

    Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,104 @@
+<template>
+    <el-container>
+      <!-- 左导航 -->
+      <el-aside width="200px" style="background-color: rgb(95, 217, 205)" >
+        <el-menu :default-openeds="['1']">
+          <el-submenu index="1">
+            <template slot="title">Layout布局</template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">Button</template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">input输入框</template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">日期选择器</template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">form表单</template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="6">
+            <template slot="title">loading加载</template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="7">
+            <template slot="title">消息提示</template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="8">
+            <template slot="title">Image 图片</template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-header style="text-align: right; font-size: 12px">
+          <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>首页</el-dropdown-item>
+              <el-dropdown-item>新增</el-dropdown-item>
+              <el-dropdown-item><router-link to="/Login">退出</router-link></el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+            <span>admin</span>
+        </el-header>
+        <el-main>
+        </el-main>
+      </el-container>
+    </el-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      list: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .el-submenu{
+    background-color: rgb(95, 217, 205);
+  }
+  .el-container{
+    height: 100%;
+  }
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
+</style>
